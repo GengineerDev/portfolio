5badeeb5a905d0d0414f47128dca791535ec16f1
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-15 10:48:23 +0800

    Update Greeting.jsx

--- web-portfolio/client/src/Components/Greeting.jsx
+++ web-portfolio/client/src/Components/Greeting.jsx
@@ -1,18 +1,18 @@
 import MainButton from './MainButton'
 function Greeting() {
     return (
-    <div class='container'>
-        <div class="column">
-          <div class="circle">
-            <div class="circle-image"></div>
+    <div className='container'>
+        <div className="column">
+          <div className="circle">
+            <div className="circle-image"></div>
         </div>
 
         </div>
-        <div class="column">
+        <div className="column">
           <h1 id="greeting-header">Hi,</h1>
           <p className='greeting-text'>Welcome to Gen's online portfolio. My hobbies range from technology to youth engagements, but I'm most excited about doing web development. View some of my work below in pictures!</p>
         </div>
-        <div class="column space-above">
+        <div className="column space-above">
           <p className='greeting-text'>If you want to read my whole experience in organizations, competitions, and whatnot, download my CV!</p>
           <MainButton link="https://docs.google.com/document/d/1w-9hkObP0lye9YG0UmeS7klA-HoFkE9I/edit?usp=share_link&ouid=114555168601342208112&rtpof=true&sd=true">See my CV!</MainButton>
         </div>
