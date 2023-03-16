bae5c7a1bdcd4e2e88d15abb2b1a80b6b9284434
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-16 19:09:57 +0800

    Edit target of vite.config.js

--- web-portfolio/client/vite.config.js
+++ web-portfolio/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-           target: 'https://localhost:5000',
+           target: 'http://localhost:5000',
            changeOrigin: true,
            secure: false,      
            ws: true,
