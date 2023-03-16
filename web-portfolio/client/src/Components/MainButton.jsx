09c6b6cd2a52f1fa8116462ebdc84d2de7932926
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-16 12:21:42 +0800

    Attempt to access handleSignIn function to button

--- web-portfolio/client/src/Components/MainButton.jsx
+++ web-portfolio/client/src/Components/MainButton.jsx
@@ -2,7 +2,7 @@ import '../styles/mainButton.css'
 
 function MainButton(props) {
   return (
-    <button className='flat-button' onClick={props.type === "special" ? console.log(props.type) : () => window.open(props.link, '_blank')}>
+    <button className='flat-button' onClick={props.type === "special" ? props.handleClick : () => window.open(props.link, '_blank')}>
       {props.children}
     </button>
   )
--- web-portfolio/client/src/pages/Login.jsx
+++ web-portfolio/client/src/pages/Login.jsx
@@ -3,12 +3,35 @@ import MainButton from '../components/MainButton'
 import '../styles/login.css'
 
 function Login() {
+
+    const handleSignIn = async () => {
+        const username = document.querySelector('input[type="text"]').value
+        const password = document.querySelector('input[type="password"]').value
+    
+        const response = await fetch('/users', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password })
+        })
+    
+        const data = await response.json()
+
+    
+        if (response.ok) {
+          console.log(data.user)
+          alert("Success!")
+          // Redirect user to /admin here
+        } else {
+          console.error(data.message)
+        }
+    }
+
     return (
         <div className="login-form">
             <img src={icon} alt="User Avatar" />
             <input type="text" placeholder="Username" />
             <input type="password" placeholder="Password" />
-            <MainButton type="special">Sign In</MainButton>
+            <MainButton type="special" handleClick={handleSignIn}>Sign In</MainButton>
         </div>
     )
 }
