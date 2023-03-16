bd4e7fcc0ff4966a968c6135e674a76271ebf2c7
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-16 12:14:58 +0800

    Update userController.js for fetching user details

--- web-portfolio/backend/controllers/userController.js
+++ web-portfolio/backend/controllers/userController.js
@@ -1,17 +1,26 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 exports.getUser = async (req, res) => {
-    const { username } = req.params;
-  
-    try {
-      const user = await User.findOne({ username });
-      if (user) {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        // Passwords match
         res.json({ success: true, user });
       } else {
-        res.status(404).json({ success: false, message: 'User not found' });
+        // Passwords don't match
+        res.status(401).json({ success: false, message: 'Invalid credentials' });
       }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ success: false, message: 'Server error' });
+    } else {
+      // User not found
+      res.status(404).json({ success: false, message: 'User not found' });
     }
-  };
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
