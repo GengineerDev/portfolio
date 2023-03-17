cb98cbdacc4f2d46b5026ddbd832da1626dea0c9
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-17 09:54:19 +0800

    Update db.js and server.js to use db config

--- web-portfolio/backend/config/db.js
+++ web-portfolio/backend/config/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+  useUnifiedTopology: true
 })
+
+module.exports = mongoose.connection
--- web-portfolio/backend/server.js
+++ web-portfolio/backend/server.js
@@ -1,6 +1,12 @@
-// server.js
 const http = require('http')
 const app = require('./app')
+const mongoose = require('mongoose')
+const db = require('./config/db')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', function() {
+  console.log('Connected to MongoDB')
+})
 
 const server = http.createServer(app)
 
