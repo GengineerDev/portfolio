edff2c7d2ea07bd619bef82c5eb3f933901bcddb
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-18 21:00:52 +0800

    Add "disabled" to MainButton component

--- web-portfolio/client/src/Components/MainButton.jsx
+++ web-portfolio/client/src/Components/MainButton.jsx
@@ -2,7 +2,7 @@ import '../styles/mainButton.css'
 
 function MainButton(props) {
   return (
-    <button className='flat-button' onClick={props.type === "special" ? props.handleClick : () => window.open(props.link, '_blank')}>
+    <button className='flat-button' onClick={props.type === "special" ? props.handleClick : () => window.open(props.link, '_blank')} disabled={props.disabled}>
       {props.children}
     </button>
   )
