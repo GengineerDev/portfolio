2e35d4c5f4063eb4ae38ec1d405105860836a7c6
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-22 16:27:42 +0800

    Initialize portfolio entry

--- web-portfolio/backend/controllers/entryController.js
+++ web-portfolio/backend/controllers/entryController.js
@@ -1,13 +1,43 @@
 const connection = require('../config/db')
 
-exports.createSession = async (req, res) => {
-  // put code here
+const db = connection.useDb('portfolio')
+const Entry = require('../models/entry')
+
+exports.createEntry = async (req, res) => {
+    const { title, thumbnail, caption, images } = req.body
+    const entry = new Entry({
+        title,
+        thumbnail,
+        caption,
+        images
+    })
+    try {
+        // save the new entry to the database
+        await entry.save()
+        res.status(201).send(entry)
+    } catch (err) {
+        res.status(400).send(err)
+    }
 }
 
-exports.getSession = async (req, res) => {
-  // put code here
+exports.getEntry = async (req, res) => {
+    // const { id } = req.params
+
+    // try {
+    //   const entry = await Entry.findById(id)
+    //   res.send(entry)
+    // } catch (err) {
+    //   res.status(400).send(err)
+    // }
 }
 
-exports.deleteSession = async (req, res) => {
-  // put code here
+exports.deleteEntry = async (req, res) => {
+    // const { id } = req.params;
+
+    // try {
+    //   const entry = await Entry.findByIdAndDelete(id)
+    //   res.send(entry)
+    // } catch (err) {
+    //   res.status(400).send(err)
+    // }
 }
