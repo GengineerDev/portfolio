df66998a35e7f7807cf9ae312d84a6116f820d4b
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-24 08:20:57 +0800

    Update Card, Tabpages, and Admin

--- web-portfolio/client/src/Components/Tabpages.jsx
+++ web-portfolio/client/src/Components/Tabpages.jsx
@@ -41,10 +41,11 @@ function Tabpages() {
             {activeTab === 'Recent Work' && (
                 <section>
                     <Card 
-                        url = "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_07/2233721/171120-smile-stock-njs-333p.jpg"
+                        thumbnail = "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_07/2233721/171120-smile-stock-njs-333p.jpg"
                         alt = "Picture of ALORA"
-                        header = "ALORA (Private repo)"
+                        title = "ALORA (Private repo)"
                         caption = "ALORA is an offline LMS made using MERN stack, RPi, router, and signal booster."
+                        images = {['https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80', 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80', 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80']}
                     />
                 
                 </section>
