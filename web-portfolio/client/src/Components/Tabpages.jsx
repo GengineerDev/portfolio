cea02c0832fba7a844badec11e5a81def0e0d0e2
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-03-24 09:01:08 +0800

    Update Tabpages.jsx

--- web-portfolio/client/src/Components/Tabpages.jsx
+++ web-portfolio/client/src/Components/Tabpages.jsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Card from './Card'
+import axios from  '../axios-config'
 import '../styles/tabpages.css'
+
 function Tabpages() {
     const [activeTab, setActiveTab] = useState('Recent Work')
+    const [entries, setEntries] = useState([])
 
     const handleTabClick = (tabName) => {
       setActiveTab(tabName)
     }
+
+    useEffect(() => {
+        axios.get(`/api/entries/${activeTab}`)
+          .then(res => setEntries(res.data))
+          .catch(err => console.error(err))
+    }, [activeTab])
+
+    const renderEntries = () => {
+        if (entries.length === 0) {
+          return <p>None found.</p>
+        } else {
+          return (
+            <section>
+              {entries.map((entry) => (
+                <Card
+                  key={entry._id}
+                  thumbnail={entry.thumbnail}
+                  alt={entry.title}
+                  title={entry.title}
+                  caption={entry.caption}
+                  images={entry.images}
+                />
+              ))}
+            </section>
+          )
+        }
+    }
+    
 
     return (
         <>
@@ -38,29 +69,7 @@ function Tabpages() {
         </div>
 
         <div className="tab-content">
-            {activeTab === 'Recent Work' && (
-                <section>
-                    <Card 
-                        thumbnail = "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_07/2233721/171120-smile-stock-njs-333p.jpg"
-                        alt = "Picture of ALORA"
-                        title = "ALORA (Private repo)"
-                        caption = "ALORA is an offline LMS made using MERN stack, RPi, router, and signal booster."
-                        images = {['https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80', 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80', 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80']}
-                    />
-                
-                </section>
-            
-            
-            )}
-            {activeTab === 'Organizations' && (
-            <p>This is the content for Organizations tab.</p>
-            )}
-            {activeTab === 'Competitions' && (
-            <p>This is the content for Competitions tab.</p>
-            )}
-            {activeTab === 'Initiatives' && (
-            <p>This is the content for Initiatives tab.</p>
-            )}
+            {renderEntries()}
         </div>
         </>
     )
